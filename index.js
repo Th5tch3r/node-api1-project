e35c7e5cad148146cc689879225ff7a98a1074fd
Th5tch3r <amber.phan27@gmail.com>
2020-01-15 14:55:19 -0500

    setting up

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+const db = require('./data/db');
+
+const express = require('express');
+
+const port = 4000;
+
+const server = express();
+
+server.listen(port, () => {
+    console.log(`This server is listening on port ${port} `);
+})
+
+server.use(express.json());
+
+//Post /api/users
