f140939b59f63f1be6bd0bb4e9835468e84fc38c
Th5tch3r <amber.phan27@gmail.com>
2020-01-15 17:22:54 -0500

    mvp done

--- index.js
+++ index.js
@@ -80,7 +80,24 @@ server.delete("/api/users/:id", (req, res) => {
         })
 })
 
-//Put /api/users/:id
+// Put /api/users/:id
 server.put("/api/users/:id", (req, res) => {
-    
+    const id = req.params.id;
+    const hubInfo = req.body;
+
+    db.update(id, hubInfo)
+        .then(user => {
+            if(user === undefined) {
+                res.status(404).json({ message: "The user with the specified ID does not exist." })
+            } else if(req.body.name === "") {
+                res.status(500).json({ errorMessage: "Please provide name and bio for the user." })
+            } else if(req.body.bio === "") {
+                res.status(500).json({ errorMessage: "Please provide name and bio for the user." })
+            } else {
+                res.status(200).json({sucess: true, message: "user updated", user})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: "The user information could not be modified.", err })
+        })
 })
